698098c20c11a73f0d53cfec98f162660b90c494
DiasMS69 <Stigmat6.9.6@gmail.com>
2024-07-03 19:03:01 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+name = "Python"
+Num_of_compl = 12
+time_work = 1.5
+Time_for_one = time_work / Num_of_compl
+print("Курс:", name, ',', "всего задач:", Num_of_compl, ',', "затрачено часов:", time_work, ',', "среднее время выполнения", Time_for_one, "часа.")
+
